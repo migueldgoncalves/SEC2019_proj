5130f3ddf29e2e29a1eefa4a558e6493b1777018
Pmsmm <pedro8745@gmail.com>
2019-03-16 03:07:24 +0000

    Signatures Made and Validated RSA Key Generation, Loading and Saving Done

--- Security/src/main/java/RSAKeyGenerator.java
+++ Security/src/main/java/RSAKeyGenerator.java
@@ -1,2 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
 public class RSAKeyGenerator {
+
+    public RSAKeyGenerator() {
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(2048);
+            KeyPair kp = kpg.generateKeyPair();
+            Key pub = kp.getPublic();
+            Key pvt = kp.getPrivate();
+
+            System.out.println("Please Insert Number of Keys To Generate:");
+            System.out.print("Number of Keys to Generate: ");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String limit = reader.readLine();
+            int max = Integer.parseInt(limit);
+
+            for (int i = 0; i < max; i++) {
+                FileOutputStream out = new FileOutputStream("User" + i + ".key");
+                out.write(pvt.getEncoded());
+                out.close();
+
+                out = new FileOutputStream("User" + i + ".pub");
+                out.write(pub.getEncoded());
+                out.close();
+
+                System.out.println("Private Key Format: " + pvt.getFormat());
+
+                System.out.println("Public Key Format: " + pub.getFormat());
+            }
+
+            System.out.println("Finished Key Generation!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
---
+++ Security/src/main/java/SignatureGenerator.java
@@ -0,0 +1,47 @@
+import java.nio.charset.StandardCharsets;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+
+public class SignatureGenerator {
+
+    public static byte[] generateSignature(PrivateKey key, String message, PublicKey pubKey) {
+        try {
+            Signature sign = Signature.getInstance("SHA512withRSA");
+
+            sign.initSign(key);
+            byte[] bytes = message.getBytes();
+
+            sign.update(bytes);
+
+            byte[] signature = sign.sign();
+
+            System.out.println("Digital Signature for given Message: " + new String(signature, StandardCharsets.UTF_8));
+
+            return signature;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void verifySignature(PublicKey key, byte[] data, String messageToVerify) {
+        try {
+            Signature sign = Signature.getInstance("SHA512withRSA");
+            sign.initVerify(key);
+            sign.update(messageToVerify.getBytes());
+
+            boolean valid = sign.verify(data);
+
+            if (valid) {
+                System.out.println("Signature verified");
+            } else {
+                System.out.println("Signature failed");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
